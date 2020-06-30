a8d63193a0c163a8fa3c8f24e88c62bcfb4499a2
ivan <ivan.perez.alarcon@utem.cl>
2020-06-29 23:45:56 -0400

    app en tablets, widget OrientationBuilder, dividir pantalla orizontal,evital scrolling o ponerlo

--- flutter_ui_avanzadas/lib/pages/login/login_page.dart
+++ flutter_ui_avanzadas/lib/pages/login/login_page.dart
@@ -10,49 +10,104 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with AfterLayoutMixin{//AfterLayoutMixin se ejecuta cuando se renderiza la app
+class _LoginPageState extends State<LoginPage> with AfterLayoutMixin {
+  //AfterLayoutMixin se ejecuta cuando se renderiza la app
 
   @override
-  void initState() {//esto se ejecuta una vez, cuando el widget es creado, si lo pusieramos en el metodo build se ejecutaria muchas veces
+  void initState() {
+    //esto se ejecuta una vez, cuando el widget es creado, si lo pusieramos en el metodo build se ejecutaria muchas veces
     super.initState();
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);//cambia la estatus bar a un color oscuro (donde se ve la bateria y hora)
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle
+        .dark); //cambia la estatus bar a un color oscuro (donde se ve la bateria y hora)
   }
 
   @override
-  void afterFirstLayout(BuildContext context){//este método se ejecuta solo 1 vez, detecta el tipo de dispositivo en el cual se ejecuta la app
+  void afterFirstLayout(BuildContext context) {
+    //este método se ejecuta solo 1 vez, detecta el tipo de dispositivo en el cual se ejecuta la app
     //para ver las medidas si está horizontal o vertical, shortestSide retorna el valor mínimo del ancho o el alto, independiente del sentido del dispositivo
-    final bool isTablet = MediaQuery.of(context).size.shortestSide >=600; //si esto se cumple es que la app está en una tablet y se permitira que el dispositivo rote, en caso contrario, se bloquea la opción de rotar la app
-    if(!isTablet){
-      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]); //SI ME ENCUENTRO EN UN SMARTPHONE SE BLOQUEA LA ROTACIÓN DEL DISPOSITIVO
+    final bool isTablet = MediaQuery.of(context).size.shortestSide >=
+        600; //si esto se cumple es que la app está en una tablet y se permitira que el dispositivo rote, en caso contrario, se bloquea la opción de rotar la app
+    if (!isTablet) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp
+      ]); //SI ME ENCUENTRO EN UN SMARTPHONE SE BLOQUEA LA ROTACIÓN DEL DISPOSITIVO
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
-    final Responsive responsive = Responsive.of(context); //llamamos a la clase Responsive que calcula las medidas del dispositivo
-    return Scaffold(
-      body: GestureDetector( //para que no salga el error de overflow al querer ingresar datos a un campo
-      //GestuoreDetector necesita un color en el container de abajo para poder detectar cuando se hace click en otra parte que no sea el campo de ingreso de datos para poder salierse si se hace click en otra parte
-      onTap: (){
-        FocusScope.of(context).unfocus();
-      },
+    final Responsive responsive = Responsive.of(
+        context); //llamamos a la clase Responsive que calcula las medidas del dispositivo
 
-      child: Container(
-          width: double.infinity,//le damos todo el alto y ancho posibles al container
+    /*
+
+    //CODIGO DE PRUEBA PARA VER LA ORIENTACIÓN DEL DISPOSITIVO, PORTRAIT(VERTICAL) O LANDSCAPE(HORIZONTAL)
+    return OrientationBuilder(builder: (_, Orientation orientation) {
+      return Center(child: Text("$orientation"));//muestra un mensaje por pantalla de la orientación del dispositivo
+    }); //con este widget se puede conocer la orientación del dispositivo
+
+    */
+
+    return Scaffold(
+      body: GestureDetector(
+        //para que no salga el error de overflow al querer ingresar datos a un campo
+        //GestuoreDetector necesita un color en el container de abajo para poder detectar cuando se hace click en otra parte que no sea el campo de ingreso de datos para poder salierse si se hace click en otra parte
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          width: double
+              .infinity, //le damos todo el alto y ancho posibles al container
           height: double.infinity,
-          color: Colors.white, //esto se agrega para que el widget GestureDetector funcione correctamente y al presionar en otra parte que no sea el campo de datos respectivo, se quita el teclado de la pantalla
-          child: SingleChildScrollView(//se usa para evitar el error de overflow al presionar un campo de inputs
-                      child: Container(
-                        height: responsive.height-30,
-                        child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,//se coloca para que los widgets Welcome() y LoginForm(), esten lo mas separados posibles
-              children: <Widget>[
-                Welcome(),
-                LoginForm(),
-              ],
-            ),
+          color: Colors
+              .white, //esto se agrega para que el widget GestureDetector funcione correctamente y al presionar en otra parte que no sea el campo de datos respectivo, se quita el teclado de la pantalla
+          child: OrientationBuilder(
+            builder: (_, Orientation orientation) {
+              if (orientation == Orientation.portrait) {
+                return SingleChildScrollView(
+                  //se usa para evitar el error de overflow al presionar un campo de inputs
+                  child: Container(
+                    height: responsive.height - 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, //se coloca para que los widgets Welcome() y LoginForm(), esten lo mas separados posibles
+                      children: <Widget>[
+                        Welcome(),
+                        LoginForm(),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: SingleChildScrollView(
+                        physics:
+                            NeverScrollableScrollPhysics(), //este atributo, bloquea el scrolling del widget welcome
+                        child: Container(
+                          padding: EdgeInsets.only(left: 30),
+                          height: responsive.height,
+                          child: Center(
+                            child: Welcome(),
+                          ),
+                        ),
                       ),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Container(
+                          height: responsive.height,
+                          child: Center(
+                            child: LoginForm(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ),
--- flutter_ui_avanzadas/lib/widgets/login_form.dart
+++ flutter_ui_avanzadas/lib/widgets/login_form.dart
@@ -6,75 +6,89 @@ import 'package:flutter_ui_avanzadas/widgets/input_text_login.dart';
 import 'package:flutter_ui_avanzadas/widgets/rounded_button.dart';
 
 class LoginForm extends StatelessWidget {
-  const LoginForm({Key key}) : super (key:key);
+  const LoginForm({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final Responsive responsive = Responsive.of(context);
     return SafeArea(
-      top: false,//ignorar el safearea de la parte superior
+      top: false, //ignorar el safearea de la parte superior
       child: Container(
         //color: Colors.red,
         width: 330,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, //hace que el CupertinoButon "forgot password"se alinea con el borde derecho
+          crossAxisAlignment: CrossAxisAlignment
+              .center, //hace que el CupertinoButon "forgot password"se alinea con el borde derecho
+          mainAxisSize: MainAxisSize
+              .min, //hace que se aline al centro la pantalla cuando está en horizontal
           children: <Widget>[
-            InputTextLogin(iconPath: 'assets/pages/login/icons/mail.svg', 
-            placeholder: "Email Adress"),
+            InputTextLogin(
+                iconPath: 'assets/pages/login/icons/mail.svg',
+                placeholder: "Email Adress"),
             SizedBox(
-              height: responsive.ip(2), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app,
+              height: responsive.ip(
+                  2), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app,
             ),
-            InputTextLogin(iconPath: 'assets/pages/login/icons/smart-key.svg', 
-            placeholder: "Password"),
+            InputTextLogin(
+                iconPath: 'assets/pages/login/icons/smart-key.svg',
+                placeholder: "Password"),
             //seccion de olvido su contraseña
             Container(
               alignment: Alignment.centerRight, //mover el texto a la derecha
-              width: double.infinity,//ocupar todo el ancho posible
+              width: double.infinity, //ocupar todo el ancho posible
               child: CupertinoButton(
-              padding: EdgeInsets.symmetric(vertical:10),
-              child: Text(
-                "Forgot Password",
-                style: TextStyle(
-                  fontFamily: 'sans',
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Forgot Password",
+                  style: TextStyle(
+                    fontFamily: 'sans',
                   ),
                 ),
-                onPressed: () {  //aquí se coloca la página o ventana de redirección deseada
+                onPressed: () {
+                  //aquí se coloca la página o ventana de redirección deseada
                 },
               ),
             ),
             SizedBox(
-              height: responsive.ip(2),//se usa para basarse en las medidas del dispositivo donde se ejecuta la app,
+              height: responsive.ip(
+                  2), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app,
             ),
             RoundedButton(
-                label: "Sign In", 
-                onPressed: () {},
-                //backgroundColor: Colors.red, //no se lo pasamos porque el color primary por defecto que agregamos está bien
+              label: "Sign In",
+              onPressed: () {},
+              //backgroundColor: Colors.red, //no se lo pasamos porque el color primary por defecto que agregamos está bien
             ),
             SizedBox(
-              height: responsive.ip(3.3), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
+              height: responsive.ip(
+                  3.3), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
             ),
             Text("Or continue with"),
             SizedBox(
-              height: responsive.ip(1), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
+              height: responsive.ip(
+                  1), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleButton(
                   size: 55,
-                  backgroundColor: Color(0xff448AFF), 
+                  backgroundColor: Color(0xff448AFF),
                   iconPath: 'assets/pages/login/icons/facebook.svg',
                 ),
-                SizedBox(width: 20,),
-                CircleButton(//botón de google
-                size: 55,
-                  backgroundColor: Color(0xffFF1744), 
+                SizedBox(
+                  width: 20,
+                ),
+                CircleButton(
+                  //botón de google
+                  size: 55,
+                  backgroundColor: Color(0xffFF1744),
                   iconPath: 'assets/pages/login/icons/google.svg',
                 ),
               ],
             ),
             SizedBox(
-              height: responsive.ip(3.7), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
+              height: responsive.ip(
+                  3.7), //se usa para basarse en las medidas del dispositivo donde se ejecuta la app
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -82,13 +96,13 @@ class LoginForm extends StatelessWidget {
                 Text("Don't have an account?"),
                 CupertinoButton(
                   child: Text(
-                    "Sign Up", 
+                    "Sign Up",
                     style: TextStyle(
-                        fontFamily: "sans",
-                        fontWeight: FontWeight.w600,//grosor del texto
-                      ),
-                  ), 
-                  onPressed: (){},
+                      fontFamily: "sans",
+                      fontWeight: FontWeight.w600, //grosor del texto
+                    ),
+                  ),
+                  onPressed: () {},
                 ),
               ],
             ),
